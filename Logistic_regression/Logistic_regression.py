29743e69c712d358a5df07b83e475dcddc59ed55
SalmonKing <hichameddoubi@gmail.com>
2022-05-13 02:38:55 +0100

    Logistic_regression files initial creation

---
+++ Logistic_regression/Logistic_regression.py
@@ -0,0 +1,13 @@
+# import pandas as pd
+# import matplotlib.pyplot as mp
+# import numpy as np
+
+# Taken from XXX
+# data = pd.read_csv('../data/XXX.csv')
+
+# Main program entry point
+def main():
+
+
+if __name__ == '__main__':
+    main()
